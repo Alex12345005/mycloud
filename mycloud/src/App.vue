4d8f7cdd42f936cc4d8d068c2086a2a9219f5d82
Alex12345005 <rath190134@sr.htlweiz.at>
2024-02-06 12:14:33 +0100

    setup simple vue frontend

---
+++ mycloud/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="uk-position-relative">
+      <div class="uk-position-top">
+        <nav class="uk-navbar-container uk-navbar-transparent">
+          <div class="uk-container">
+            <div uk-navbar>
+
+              <div class="uk-navbar-left">
+                <ul class="uk-navbar-nav">
+                  <RouterLink to="/">Home</RouterLink>
+                  <RouterLink to="/about">About</RouterLink>
+                  <!-- Fügen Sie hier weitere Navigationslinks hinzu, falls gewünscht -->
+                </ul>
+              </div>
+
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+/* Stilisierung Ihrer Elemente */
+</style>
---
+++ mycloud/src/components/DisplayFolders.vue
@@ -0,0 +1,47 @@
+<!-- App.vue -->
+
+<template>
+    <div>
+      <h1>Ordner und Dateien</h1>
+      <div class="folders">
+        <div
+          class="folder"
+          v-for="folder in folders"
+          :key="folder.id"
+          @click="showFiles(folder.id)"
+        >
+          {{ folder.name }}
+        </div>
+      </div>
+      <div class="files">
+        <div class="file" v-for="file in files" :key="file.id">
+          {{ file.name }}
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        folders: [],
+        files: [],
+      };
+    },
+    methods: {
+      async showFiles(folderId) {
+        // Hier sollte die Logik implementiert werden, um Dateien in einem Ordner anzuzeigen
+        // Rufen Sie die Dateien für den ausgewählten Ordner von Ihrer API ab und aktualisieren Sie die 'files'-Daten
+      },
+    },
+    created() {
+      // Hier sollten Sie die Ordner beim Laden der Seite von Ihrer API abrufen und die 'folders'-Daten aktualisieren
+    },
+  };
+  </script>
+  
+  <style>
+  /* Fügen Sie hier Ihre CSS-Stile hinzu, um die Ordner und Dateien anzuzeigen */
+  </style>
+  
